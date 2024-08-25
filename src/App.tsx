d902c0aa0917ab392e3e6bf2e630d4621407ef15
amirrh6 <amirrezahh6@gmail.com>
2024-08-26 00:11:19 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
                 </p>
 
                 <div className="avatar sm:float-end">
-                    <div className="w-24 rounded">
+                    <div className="w-24 mt-2 rounded">
                         <img src="https://media.licdn.com/dms/image/v2/D4D03AQHoqpdwMBVN6A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724595551392?e=1730332800&v=beta&t=WnkQss2mcsOnql8OEgWud_Ut0-jxhN9SpfqjdgIne9o" />
                     </div>
                 </div>
